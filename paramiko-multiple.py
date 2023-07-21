676fd5f8f37ffdfe5b34332e9f65e1297000ab8a
Naima Hassan <naima.hassan434@gmail.com>
2023-07-21 21:27:50 +0100

    Automating the configuration ospf of multiple cisco Routers

--- paramiko-multiple.py
+++ paramiko-multiple.py
@@ -5,25 +5,34 @@ import time
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-router = {'hostname': '10.1.1.10', 'port': '22', 'username': 'naim', 'password':'cisco'}
-print(f'Connecting to {router["hostname"]}')
+# creating dictionary for each device to connect to
 
-ssh_client.connect(**router, look_for_keys=False, allow_agent=False)
-shell = ssh_client.invoke_shell()
+router1 = {'hostname': '10.1.1.10', 'port': '22', 'username': 'naim', 'password':'cisco'}
+router2 = {'hostname': '10.1.1.20', 'port': '22', 'username': 'naim', 'password':'cisco'}
+router3 = {'hostname': '10.1.1.30', 'port': '22', 'username': 'naim', 'password':'cisco'}
 
-shell.send('enable\n')
-# shell.send('cisco\n')
-shell.send('conf t\n')
-shell.send('router ospf 1\n')
-shell.send('network 0.0.0.0 0.0.0.0 area 0\n')
-shell.send('end\n')
-shell.send('terminal length 0\n')
-shell.send('show ip protocols\n')
+# creating a list of dictionaries (of devices)
+routers = [router1, router2, router3]
 
-time.sleep(2)
+for router in routers:
+    print(f'Connecting to {router["hostname"]}')
 
-output = shell.recv(10000).decode()
-print(output)
+    ssh_client.connect(**router, look_for_keys=False, allow_agent=False)
+    shell = ssh_client.invoke_shell()
 
-if ssh_client.get_transport().is_active() == True:
-    print('Closing connection')
+    shell.send('enable\n')
+    # shell.send('cisco\n')
+    shell.send('conf t\n')
+    shell.send('router ospf 1\n')
+    shell.send('network 0.0.0.0 0.0.0.0 area 0\n')
+    shell.send('end\n')
+    shell.send('terminal length 0\n')
+    shell.send('show ip protocols\n')
+
+    time.sleep(2)
+
+    output = shell.recv(10000).decode()
+    print(output)
+
+    if ssh_client.get_transport().is_active() == True:
+        print('Closing connection')
